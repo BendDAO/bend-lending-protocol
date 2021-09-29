4ec2a47d99e4c0bc3e6df4b1458648176e0fef73
wenyuangui <wenyuangui@yeah.net>
2021-09-29 16:58:33 +0800

    add WToken contract

--- contracts/interfaces/ILendPool.sol
+++ contracts/interfaces/ILendPool.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
+import {DataTypes} from "../libraries/types/DataTypes.sol";
+
 interface ILendPool {
     /**
      * @dev Emitted on deposit()
@@ -196,6 +198,47 @@ interface ILendPool {
         uint256 tokenId,
         address user
     ) external;
+
+    /**
+     * @dev Returns the normalized income normalized income of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The reserve's normalized income
+     */
+    function getReserveNormalizedIncome(address asset)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Returns the normalized variable debt per unit of asset
+     * @param asset The address of the underlying asset of the reserve
+     * @return The reserve normalized variable debt
+     */
+    function getReserveNormalizedVariableDebt(address asset)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Returns the state and configuration of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The state of the reserve
+     **/
+    function getReserveData(address asset)
+        external
+        view
+        returns (DataTypes.ReserveData memory);
+
+    function finalizeTransfer(
+        address asset,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 balanceFromAfter,
+        uint256 balanceToBefore
+    ) external;
+
+    function getReservesList() external view returns (address[] memory);
 
     function setPause(bool val) external;
 
