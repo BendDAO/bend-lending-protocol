34ed803ee00e9c4025cbfec62092a1f6148c337c
Thanos Wen <thanoswen@protonmail.com>
2021-09-29 15:12:29 +0800

    rewrite NFTLoan contract

--- contracts/protocol/NFTLoan.sol
+++ contracts/protocol/NFTLoan.sol
@@ -4,29 +4,25 @@ pragma solidity ^0.8.0;
 import "../interfaces/INFTLoan.sol";
 import "../interfaces/ILendPool.sol";
 import "../libraries/helpers/Errors.sol";
+import "../libraries/types/DataTypes.sol";
 import {WadRayMath} from "../libraries/math/WadRayMath.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTLoan is INFTLoan, ERC721 {
     using WadRayMath for uint256;
+    using Counters for Counters.Counter;
 
-    address nftToken;
-    ILendPool _pool;
+    ILendPool private _pool;
 
-    struct Loan {
-        uint256 tokenId;
-        address wToken;
-        uint256 amount;
-        uint64 loanStartTime;
-    }
-
-    mapping(uint256 => Loan) public loans;
+    Counters.Counter private _loanIdTracker;
+    mapping(uint256 => DataTypes.LoanData) private _loans;
 
     /**
      * @dev Only lending pool can call functions marked by this modifier
      **/
-    modifier onlyLendingPool() {
+    modifier onlyLendPool() {
         require(
             _msgSender() == address(_getLendPool()),
             Errors.CT_CALLER_MUST_BE_LENDING_POOL
@@ -34,92 +30,141 @@ contract NFTLoan is INFTLoan, ERC721 {
         _;
     }
 
-    constructor() ERC721("NFTDebtToken", "NFTD") {}
+    constructor() ERC721("NFTLoan", "NFTL") {}
 
     // called once by the factory at time of deployment
-    function initialize(address _nft, address _lendPool) external {
-        nftToken = _nft;
+    function initialize(address _lendPool) external {
         _pool = ILendPool(_lendPool);
     }
 
     /**
-     * @dev Mints debt token to the `onBehalfOf` address
+     * @dev Mints loan to the `user` address
      * -  Only callable by the LendingPool
-     * @param user The address receiving the borrowed underlying, being the delegatee in case
-     * of credit delegate, or same as `onBehalfOf` otherwise
-     * @param onBehalfOf The address receiving the debt tokens
+     * @param user The address receiving the loan
      * @param amount The amount of debt being minted
-     * @param index The variable debt index of the reserve
+     * @param borrowIndex The variable borrow index of the reserve
      **/
-    function mint(
+    function mintLoan(
         address user,
-        address onBehalfOf,
-        uint256 tokenId,
-        address wToken,
+        address nftTokenAddress,
+        uint256 nftTokenId,
+        address assetAddress,
         uint256 amount,
-        uint256 index
-    ) external onlyLendingPool {
-        uint256 amountScaled = amount.rayDiv(index);
+        uint256 borrowIndex
+    ) external override onlyLendPool returns (uint256 loanId) {
+        uint256 amountScaled = amount.rayDiv(borrowIndex);
 
-        if (_exists(tokenId)) {
-            Loan memory loan = loans[tokenId];
-            require(loan.wToken == wToken, "NFTLoan: NONEXIST_ORDER");
-            loan.amount += amountScaled;
-        } else {
-            // Receive Collateral Tokens
-            IERC721(nftToken).transferFrom(msg.sender, address(this), tokenId);
+        loanId = _loanIdTracker.current();
+        _loanIdTracker.increment();
 
-            // Save Info
-            loans[tokenId] = Loan({
-                tokenId: tokenId,
-                wToken: wToken,
-                amount: amount,
-                loanStartTime: uint64(block.timestamp)
-            });
+        // Receive Collateral Tokens
+        IERC721(nftTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            nftTokenId
+        );
 
-            _mint(onBehalfOf, tokenId);
-        }
+        // Save Info
+        _loans[loanId] = DataTypes.LoanData({
+            loanId: loanId,
+            nftTokenAddress: nftTokenAddress,
+            nftTokenId: nftTokenId,
+            assetAddress: assetAddress,
+            scaledAmount: amountScaled
+        });
 
-        emit MintLoan(user, onBehalfOf, tokenId, wToken, amount, index);
+        _mint(user, loanId);
+
+        emit MintLoan(
+            user,
+            nftTokenAddress,
+            nftTokenId,
+            assetAddress,
+            amount,
+            borrowIndex
+        );
     }
 
     /**
      * @dev Burns user variable debt
      * - Only callable by the LendingPool
      * @param user The user whose debt is getting burned
-     * @param amount The amount getting burned
-     * @param index The variable debt index of the reserve
+     * @param loanId The id of loan
+     * @param borrowIndex The variable debt index of the reserve
      **/
-    function burn(
+    function burnLoan(
         address user,
-        uint256 tokenId,
-        uint256 amount,
-        uint256 index
-    ) external onlyLendingPool {
-        require(_exists(tokenId), "NFTLoan: NONEXIST_ORDER");
-        require(ownerOf(tokenId) == msg.sender, "NFTLoan: NOT_OWNER");
+        uint256 loanId,
+        uint256 borrowIndex
+    ) external override onlyLendPool {
+        require(_exists(loanId), "NFTLoan: nonexist loan");
 
-        Loan memory loan = loans[tokenId];
+        DataTypes.LoanData memory loan = _loans[loanId];
 
-        uint256 amountScaled = 0;
-        if (amount == type(uint256).max) {
-            amountScaled = loan.amount;
-        } else {
-            amountScaled = amount.rayDiv(index);
-            require(amountScaled != 0, "NFTLoan: invalid burn amount");
+        IERC721(loan.nftTokenAddress).transferFrom(
+            address(this),
+            msg.sender,
+            loan.nftTokenId
+        );
+
+        _burn(loanId);
+
+        delete _loans[loanId];
+
+        emit BurnLoan(
+            user,
+            loanId,
+            loan.nftTokenAddress,
+            loan.nftTokenId,
+            loan.assetAddress,
+            loan.scaledAmount
+        );
+    }
+
+    function updateLoan(
+        address user,
+        uint256 loanId,
+        uint256 amountAdded,
+        uint256 amountTaken,
+        uint256 borrowIndex
+    ) external override onlyLendPool {
+        require(_exists(loanId), "NFTLoan: nonexist loan");
+
+        DataTypes.LoanData memory loan = _loans[loanId];
+
+        if (amountAdded > 0) {
+            uint256 amountScaled = amountAdded.rayDiv(borrowIndex);
+            require(amountScaled != 0, "NFTLoan: invalid added amount");
+            loan.scaledAmount += amountScaled;
         }
 
-        require(loan.amount >= amountScaled, "NFTLoan: burn amount exceeds");
-        loan.amount -= amountScaled;
-        if (loan.amount <= 0) {
-            IERC721(nftToken).transferFrom(address(this), msg.sender, tokenId);
-
-            _burn(tokenId);
-
-            delete loans[tokenId];
+        if (amountTaken > 0) {
+            uint256 amountScaled = amountTaken.rayDiv(borrowIndex);
+            require(amountScaled != 0, "NFTLoan: invalid taken amount");
+            require(
+                loan.scaledAmount >= amountScaled,
+                "NFTLoan: taken amount exceeds"
+            );
+            loan.scaledAmount -= amountScaled;
         }
 
-        emit BurnLoan(user, tokenId, amount, index);
+        emit UpdateLoan(
+            user,
+            loanId,
+            loan.assetAddress,
+            amountAdded,
+            amountTaken,
+            borrowIndex
+        );
+    }
+
+    function getLoan(uint256 loanId)
+        external
+        view
+        override
+        returns (DataTypes.LoanData memory loanData)
+    {
+        return _loans[loanId];
     }
 
     function _getLendPool() internal view returns (ILendPool) {
