dfe0cd9eeb68c28d25c0f0303a1ae3f1d3318b96
wenyuangui <wenyuangui@yeah.net>
2021-09-29 15:12:29 +0800

    rewrite NFTLoan contract

--- contracts/interfaces/INFTLoan.sol
+++ contracts/interfaces/INFTLoan.sol
@@ -1,33 +1,77 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
+import "../libraries/types/DataTypes.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface INFTLoan is IERC721 {
     /**
-     * @dev Emitted on mint()
+     * @dev Emitted on mintLoan()
      * @param user The address initiating the deposit
-     * @param onBehalfOf The beneficiary of the deposit, receiving the aTokens
      * @param amount The amount minted
      **/
     event MintLoan(
-        address user,
-        address indexed onBehalfOf,
-        uint256 tokenId,
-        address indexed wToken,
+        address indexed user,
+        address nftTokenAddress,
+        uint256 nftTokenId,
+        address assetAddress,
         uint256 amount,
-        uint256 index
+        uint256 borrowIndex
     );
 
     /**
-     * @dev Emitted on burn()
+     * @dev Emitted on burnLoan()
      * @param user The address initiating the burn
      * @param amount The amount burned
      **/
     event BurnLoan(
         address indexed user,
-        uint256 tokenId,
-        uint256 amount,
-        uint256 index
+        uint256 indexed loanId,
+        address nftTokenAddress,
+        uint256 nftTokenId,
+        address assetAddress,
+        uint256 amount
     );
+
+    /**
+     * @dev Emitted on updateLoan()
+     * @param user The address initiating the burn
+     * @param amountAdded The amount added
+     **/
+    event UpdateLoan(
+        address indexed user,
+        uint256 indexed loanId,
+        address assetAddress,
+        uint256 amountAdded,
+        uint256 amountTaken,
+        uint256 borrowIndex
+    );
+
+    function mintLoan(
+        address user,
+        address nftTokenAddress,
+        uint256 nftTokenId,
+        address assetAddress,
+        uint256 amount,
+        uint256 borrowIndex
+    ) external returns (uint256 loanId);
+
+    function burnLoan(
+        address user,
+        uint256 loanId,
+        uint256 borrowIndex
+    ) external;
+
+    function updateLoan(
+        address user,
+        uint256 loanId,
+        uint256 amountAdded,
+        uint256 amountTaken,
+        uint256 borrowIndex
+    ) external;
+
+    function getLoan(uint256 loanId)
+        external
+        view
+        returns (DataTypes.LoanData calldata loanData);
 }
