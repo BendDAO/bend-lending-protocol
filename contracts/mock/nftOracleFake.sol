e2b827ab4aa1a9fed1f093fe4544b96551fab034
Thanos Wen <thanoswen@protonmail.com>
2021-10-14 08:21:59 +0800

    add nft oracle
    Signed-off-by: pwc5232759 <659672152@qq.com>

---
+++ contracts/mock/nftOracleFake.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+import {NFTOracle} from "../protocol/NFTOracle.sol";
+
+contract NFTOracleFake is NFTOracle {
+    uint256 private timestamp = 1444004400;
+    uint256 private number = 10001;
+
+    function mock_setBlockTimestamp(uint256 _timestamp) public {
+        timestamp = _timestamp;
+    }
+
+    function mock_setBlockNumber(uint256 _number) public {
+        number = _number;
+    }
+
+    function mock_getCurrentTimestamp() public view returns (uint256) {
+        return _blockTimestamp();
+    }
+
+    // Override BlockContext here
+    function _blockTimestamp() internal view override returns (uint256) {
+        return timestamp;
+    }
+
+    function _blockNumber() internal view override returns (uint256) {
+        return number;
+    }
+}
---
+++ contracts/utils/BlockContext.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+// wrap block.xxx functions for testing
+// only support timestamp and number so far
+abstract contract BlockContext {
+    //◥◤◥◤◥◤◥◤◥◤◥◤◥◤◥◤ add state variables below ◥◤◥◤◥◤◥◤◥◤◥◤◥◤◥◤//
+
+    //◢◣◢◣◢◣◢◣◢◣◢◣◢◣◢◣ add state variables above ◢◣◢◣◢◣◢◣◢◣◢◣◢◣◢◣//
+    uint256[50] private __gap;
+
+    function _blockTimestamp() internal view virtual returns (uint256) {
+        return block.timestamp;
+    }
+
+    function _blockNumber() internal view virtual returns (uint256) {
+        return block.number;
+    }
+}
