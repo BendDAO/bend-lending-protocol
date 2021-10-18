69f99e20cb96711faeb6360c3b15f80ed2ef543c
Thanos Wen <thanoswen@protonmail.com>
2021-10-18 18:12:45 +0800

    add more testcase
    Signed-off-by: pwc5232759 <659672152@qq.com>

--- test/NFTOracle.test.js
+++ test/NFTOracle.test.js
@@ -3,7 +3,6 @@ const { ethers } = require("hardhat");
 const { expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
 
 describe("NFTOralce", () => {
-    let NFTOracle;
     let nftOracleInstance;
     let owner;
     let addr1;
@@ -11,7 +10,7 @@ describe("NFTOralce", () => {
     let addrs
 
     beforeEach(async () => {
-        NFTOracle = await ethers.getContractFactory("NFTOracleFake");
+        let NFTOracle = await ethers.getContractFactory("NFTOracleFake");
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
         nftOracleInstance = await NFTOracle.deploy();
         await nftOracleInstance.deployed();
@@ -19,6 +18,7 @@ describe("NFTOralce", () => {
     });
 
     it("add Asset", async () => {
+        expect(await nftOracleInstance.owner()).to.equal(owner.address);
         await nftOracleInstance.addAsset(addr1.address)
         expect(await nftOracleInstance.nftPriceFeedKeys(0)).eq(addr1.address)
     });
@@ -93,16 +93,22 @@ describe("NFTOralce", () => {
         await nftOracleInstance.setAssetData(addr1.address, 420, currentTime + 45, 102)
 
         await nftOracleInstance.removeAsset(addr1.address)
-
-        await expectRevert(nftOracleInstance.getAssetPrice(addr1.address), "key not existed",)
+        await expect(nftOracleInstance.getAssetPrice(addr1.address)).to.be.revertedWith(
+            "key not existed"
+        );
+        await expect(nftOracleInstance.getLatestTimestamp(addr1.address)).to.be.revertedWith(
+            "key not existed"
+        );
+        //await expectRevert(nftOracleInstance.getAssetPrice(addr1.address), "key not existed")
         // await expectRevert(nftOracleInstance.getLatestTimestamp(addr1.address), "key not existed")
     })
 
     it("round id can be the same", async () => {
+        const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
         await nftOracleInstance.addAsset(addr1.address)
-        await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100)
-        await nftOracleInstance.setAssetData(addr1.address, 400, 1444004430, 100)
-        await expectEvent.inTransaction(r.tx, nftOracleInstance, "SetAssetData")
+        await nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 15, 100)
+        await nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 30, 100)
+        //await expectEvent.inTransaction(r.tx, nftOracleInstance, "SetAssetData")
     })
 
     it("force error, get data with no price feed data", async () => {
@@ -111,86 +117,117 @@ describe("NFTOralce", () => {
         expect(await nftOracleInstance.getPriceFeedLength(addr1.address)).to.equal('0')
         expect(await nftOracleInstance.getLatestTimestamp(addr1.address)).to.equal('0')
 
-        await expectRevert(nftOracleInstance.getAssetPrice(addr1.address), "no price data")
-        await expectRevert(nftOracleInstance.getTwapPrice(addr1.address, 1), "Not enough history")
-        await expectRevert(nftOracleInstance.getPreviousPrice(addr1.address, 0), "Not enough history")
-        await expectRevert(nftOracleInstance.getPreviousTimestamp(addr1.address, 0), "Not enough history")
+        await expect(nftOracleInstance.getAssetPrice(addr1.address)).to.be.revertedWith(
+            "no price data"
+        )
+        await expect(nftOracleInstance.getTwapPrice(addr1.address, 1)).to.be.revertedWith(
+            "Not enough history"
+        )
+        await expect(nftOracleInstance.getPreviousPrice(addr1.address, 0)).to.be.revertedWith(
+            "Not enough history"
+        )
+        await expect(nftOracleInstance.getPreviousTimestamp(addr1.address, 0)).to.be.revertedWith(
+            "Not enough history"
+        )
+        // await expectRevert(nftOracleInstance.getAssetPrice(addr1.address), "no price data")
+        // await expectRevert(nftOracleInstance.getTwapPrice(addr1.address, 1), "Not enough history")
+        // await expectRevert(nftOracleInstance.getPreviousPrice(addr1.address, 0), "Not enough history")
+        // await expectRevert(nftOracleInstance.getPreviousTimestamp(addr1.address, 0), "Not enough history")
     })
 
     it("force error, asset should be set first", async () => {
-        await expectRevert(
-            nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100),
-            "key not existed",
+        // await expectRevert(
+        //     nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100),
+        //     "key not existed",
+        // )
+        const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
+        await expect(nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 15, 100)).to.be.revertedWith(
+            "key not existed"
         )
     })
 
     it("force error, timestamp should be larger", async () => {
+        const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
         await nftOracleInstance.addAsset(addr1.address)
-        await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100)
-        await expectRevert(
-            await nftOracleInstance.setAssetData(addr1.address, 400, 1444004400, 100),
-            "incorrect timestamp",
+        await nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 15, 100)
+        // await expectRevert(
+        //     await nftOracleInstance.setAssetData(addr1.address, 400, 1444004400, 100),
+        //     "incorrect timestamp",
+        // )
+        await expect(nftOracleInstance.setAssetData(addr1.address, 400, currentTime, 100)).to.be.revertedWith(
+            "incorrect timestamp"
         )
     })
 
     it("force error, timestamp can't be the same", async () => {
+        const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
         await nftOracleInstance.addAsset(addr1.address)
-        await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100)
-        await expectRevert(
-            await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 101),
-            "incorrect timestamp",
+        await nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 15, 100)
+        // await expectRevert(
+        //     await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 101),
+        //     "incorrect timestamp",
+        // )
+        await expect(nftOracleInstance.setAssetData(addr1.address, 400, currentTime + 15, 101)).to.be.revertedWith(
+            "incorrect timestamp"
         )
     })
 
     describe("twap", () => {
         beforeEach(async () => {
             await nftOracleInstance.addAsset(addr1.address)
-
-            await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100)
-            await nftOracleInstance.setAssetData(addr1.address, 405, 1444004430, 101)
-            await nftOracleInstance.setAssetData(addr1.address, 410, 1444004445, 102)
+            const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(15))
+            await nftOracleInstance.setAssetData(addr1.address, 4000000000000000, currentTime.add(15), 100)
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(30))
+            await nftOracleInstance.setAssetData(addr1.address, 4050000000000000, currentTime.add(30), 101)
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(45))
+            await nftOracleInstance.setAssetData(addr1.address, 4100000000000000, currentTime.add(45), 102)
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(60))
         })
 
         it("twap price", async () => {
+            // (15*4100000000000000+15*4050000000000000+15*4000000000000000)/45 = 405
             const price = await nftOracleInstance.getTwapPrice(addr1.address, 45)
-            expect(price).to.equal("405")
+            expect(price).to.equal("4050000000000000")
         })
 
         it("asking interval more than asset has", async () => {
+            // (15*4100000000000000+15*4050000000000000+15*4000000000000000)/45 = 405
             const price = await nftOracleInstance.getTwapPrice(addr1.address, 46)
-            expect(price).to.equal("405")
+            expect(price).to.equal("4050000000000000")
         })
 
         it("asking interval less than asset has", async () => {
-            const price = await nftOracleInstance.getTwapPrice(toBytes32("ETH"), 44)
-            expect(price).to.equal("405113636363636363636")
+            // (15*4100000000000000+15*4050000000000000+14*4000000000000000)/44 = 4051136363636363
+            const price = await nftOracleInstance.getTwapPrice(addr1.address, 44)
+            expect(price).to.equal("4051136363636363")
         })
 
         it("given variant price period", async () => {
-            const currentTime = await this.l2PriceFeed.mock_getCurrentTimestamp()
-            await this.l2PriceFeed.mock_setBlockTimestamp(currentTime.addn(30))
-            await this.l2PriceFeed.setLatestData(toBytes32("ETH"), toFullDigit(420), currentTime.addn(30), 4)
-            await this.l2PriceFeed.mock_setBlockTimestamp(currentTime.addn(50))
-
-            await nftOracleInstance.setAssetData(addr1.address, 420, 1444004490, 103)
+            const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(30))
+            await nftOracleInstance.setAssetData(addr1.address, 4200000000000000, currentTime.add(30), 103)
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime.add(50))
 
             // twap price should be (400 * 15) + (405 * 15) + (410 * 45) + (420 * 20) / 95 = 409.74
             const price = await nftOracleInstance.getTwapPrice(addr1.address, 95)
-            expect(price).to.equal("409736842105263157894")
+            expect(price).to.equal("4097368421052631")
         })
 
         it("latest price update time is earlier than the request, return the latest price", async () => {
-            const currentTime = await this.l2PriceFeed.mock_getCurrentTimestamp()
-            await this.l2PriceFeed.mock_setBlockTimestamp(currentTime.addn(100))
+            const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
+            await nftOracleInstance.mock_setBlockTimestamp(currentTime + 100)
 
             // latest update time is base + 30, but now is base + 145 and asking for (now - 45)
             // should return the latest price directly
-            const price = await this.l2PriceFeed.getTwapPrice(toBytes32("ETH"), 45)
-            assert.equal(price.valueOf(), toFullDigitStr(410))
+            const price = await nftOracleInstance.getTwapPrice(addr1.address, 45)
+            expect(price).to.equal("4100000000000000")
         })
 
         it("get 0 while interval is zero", async () => {
-            await expectRevert(nftOracleInstance.getTwapPrice(addr1.address, 0), "interval can't be 0")
+            await expect(nftOracleInstance.getTwapPrice(addr1.address, 0)).to.be.revertedWith(
+                "interval can't be 0"
+            )
         })
     })
 
@@ -198,22 +235,23 @@ describe("NFTOralce", () => {
         let baseTimestamp
         beforeEach(async () => {
             await nftOracleInstance.addAsset(addr1.address)
-
-            await nftOracleInstance.setAssetData(addr1.address, 400, 1444004415, 100)
-            await nftOracleInstance.setAssetData(addr1.address, 410, 1444004430, 101)
-            await nftOracleInstance.setAssetData(addr1.address, 420, 1444004445, 102)
+            const currentTime = await nftOracleInstance.mock_getCurrentTimestamp()
+            baseTimestamp = currentTime
+            await nftOracleInstance.setAssetData(addr1.address, 400, currentTime.add(15), 100)
+            await nftOracleInstance.setAssetData(addr1.address, 410, currentTime.add(30), 101)
+            await nftOracleInstance.setAssetData(addr1.address, 420, currentTime.add(45), 102)
         })
 
         it("get previous price (latest)", async () => {
             const price = await nftOracleInstance.getPreviousPrice(addr1.address, 0)
             expect(price).to.equal("420")
             const timestamp = await nftOracleInstance.getPreviousTimestamp(addr1.address, 0)
-            expect(timestamp).to.equal("1444004445")
+            expect(timestamp).to.equal(baseTimestamp.add(45)).toString()
 
-            const price = await nftOracleInstance.getPreviousPrice(addr1.address, 0)
-            assert.equal(price.valueOf(), toFullDigitStr(410))
-            const timestamp = await this.l2PriceFeed.getPreviousTimestamp(toBytes32("ETH"), 0)
-            assert.equal(timestamp.valueOf(), (baseTimestamp.addn(45)).toString())
+            const price1 = await nftOracleInstance.getPreviousPrice(addr1.address, 1)
+            expect(price1).to.equal("410")
+            const timestamp1 = await nftOracleInstance.getPreviousTimestamp(addr1.address, 1)
+            expect(timestamp1).to.equal(baseTimestamp.add(30)).toString()
         })
 
         it("get previous price", async () => {
@@ -224,8 +262,12 @@ describe("NFTOralce", () => {
         })
 
         it("force error, get previous price", async () => {
-            await expectRevert(nftOracleInstance.getPreviousPrice(addr1.address, 3), "Not enough history")
-            await expectRevert(nftOracleInstance.getPreviousTimestamp(addr1.address, 3), "Not enough history")
+            await expect(nftOracleInstance.getPreviousPrice(addr1.address, 3)).to.be.revertedWith(
+                "Not enough history"
+            )
+            await expect(nftOracleInstance.getPreviousTimestamp(addr1.address, 3)).to.be.revertedWith(
+                "Not enough history"
+            )
         })
     })
 });
