def283e7deced7e9f217cbcf385caf432709a516
Thanos Wen <thanoswen@protonmail.com>
2021-10-19 11:31:46 +0800

    add reserve oracle contract
    Signed-off-by: pwc5232759 <659672152@qq.com>

--- contracts/interfaces/IReserveOracle.sol
+++ contracts/interfaces/IReserveOracle.sol
@@ -5,13 +5,15 @@ pragma solidity ^0.8.0;
 @title IReserveOracle interface
 @notice Interface for Reserve price oracle.*/
 interface IReserveOracle {
-    /***********
-    @dev returns the asset price in ETH
-     */
-    function getAssetPrice(address asset) external view returns (uint256);
+    // get latest price
+    function getAssetPrice(bytes32 _priceFeedKey)
+        external
+        view
+        returns (uint256);
 
-    /***********
-    @dev sets the asset price, in wei
-     */
-    function setAssetPrice(address asset, uint256 price) external;
+    // get twap price depending on _period
+    function getTwapPrice(bytes32 _priceFeedKey, uint256 _interval)
+        external
+        view
+        returns (uint256);
 }
