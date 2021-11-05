ec503f43423ddcece96ab0a85148749bb8036c8c
Thanos Wen <thanoswen@protonmail.com>
2021-11-05 19:11:40 +0800

    rename BYAC to BAYC

--- test/bnft-flashloan.spec.ts
+++ test/bnft-flashloan.spec.ts
@@ -20,18 +20,18 @@ makeSuite("BNFT: FlashLoan function", (testEnv: TestEnv) => {
   let user1TokenId1 = {} as string;
 
   before(async () => {
-    const { bayc, bBYAC, bnftRegistry } = testEnv;
+    const { bayc, bBAYC, bnftRegistry } = testEnv;
 
     _mockFlashLoanReceiver = await deployMockFlashLoanReceiver([bnftRegistry.address]);
-    _mockBNFTMinter = await deployMockBNFTMinter([bayc.address, bBYAC.address]);
+    _mockBNFTMinter = await deployMockBNFTMinter([bayc.address, bBAYC.address]);
   });
 
   afterEach(async () => {
     await _mockFlashLoanReceiver.clearAllSimulate();
   });
 
-  it("Mints BAYC into the BNFT", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Mints NFT into the BNFT", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
     const userAddress = await pool.signer.getAddress();
 
     testEnv.tokenIdTracker++;
@@ -56,131 +56,131 @@ makeSuite("BNFT: FlashLoan function", (testEnv: TestEnv) => {
     await _mockBNFTMinter.mint(users[1].address, user1TokenId1);
   });
 
-  it("Takes BAYC flashloan using one token, returns the tokens correctly", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Takes flashloan using one token, returns the tokens correctly", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     const ownerBefore = await bayc.ownerOf(user0TokenId1);
-    expect(ownerBefore).to.be.equal(bBYAC.address);
-    const ownerBeforeB = await bBYAC.ownerOf(user0TokenId1);
+    expect(ownerBefore).to.be.equal(bBAYC.address);
+    const ownerBeforeB = await bBAYC.ownerOf(user0TokenId1);
     expect(ownerBeforeB).to.be.equal(users[0].address);
 
-    await bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], []);
+    await bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], []);
 
     const ownerAfter = await bayc.ownerOf(user0TokenId1);
-    expect(ownerAfter).to.be.equal(bBYAC.address);
-    const ownerAfterB = await bBYAC.ownerOf(user0TokenId1);
+    expect(ownerAfter).to.be.equal(bBAYC.address);
+    const ownerAfterB = await bBAYC.ownerOf(user0TokenId1);
     expect(ownerAfterB).to.be.equal(users[0].address);
   });
 
-  it("Takes BYAC flashloan using many tokens, returns the tokens correctly", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Takes flashloan using many tokens, returns the tokens correctly", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     const ownerBefore1 = await bayc.ownerOf(user0TokenId1);
-    expect(ownerBefore1).to.be.equal(bBYAC.address);
-    const ownerBeforeB1 = await bBYAC.ownerOf(user0TokenId1);
+    expect(ownerBefore1).to.be.equal(bBAYC.address);
+    const ownerBeforeB1 = await bBAYC.ownerOf(user0TokenId1);
     expect(ownerBeforeB1).to.be.equal(users[0].address);
 
     const ownerBefore2 = await bayc.ownerOf(user0TokenId2);
-    expect(ownerBefore2).to.be.equal(bBYAC.address);
-    const ownerBeforeB2 = await bBYAC.ownerOf(user0TokenId2);
+    expect(ownerBefore2).to.be.equal(bBAYC.address);
+    const ownerBeforeB2 = await bBAYC.ownerOf(user0TokenId2);
     expect(ownerBeforeB2).to.be.equal(users[0].address);
 
-    await bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1, user0TokenId2], []);
+    await bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1, user0TokenId2], []);
 
     const ownerAfter1 = await bayc.ownerOf(user0TokenId1);
-    expect(ownerAfter1).to.be.equal(bBYAC.address);
-    const ownerAfterB1 = await bBYAC.ownerOf(user0TokenId1);
+    expect(ownerAfter1).to.be.equal(bBAYC.address);
+    const ownerAfterB1 = await bBAYC.ownerOf(user0TokenId1);
     expect(ownerAfterB1).to.be.equal(users[0].address);
 
     const ownerAfter2 = await bayc.ownerOf(user0TokenId2);
-    expect(ownerAfter2).to.be.equal(bBYAC.address);
-    const ownerAfterB2 = await bBYAC.ownerOf(user0TokenId2);
+    expect(ownerAfter2).to.be.equal(bBAYC.address);
+    const ownerAfterB2 = await bBAYC.ownerOf(user0TokenId2);
     expect(ownerAfterB2).to.be.equal(users[0].address);
   });
 
-  it("Takes BAYC flashloan, does not return all the tokens. (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Takes flashloan, does not return all the tokens. (revert expected)", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setTokenIdNotToApprove(user0TokenId1);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
   });
 
-  it("Takes BAYC flashloan, does not return partly the tokens. (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Takes flashloan, does not return partly the tokens. (revert expected)", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setTokenIdNotToApprove(user0TokenId2);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1, user0TokenId2], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1, user0TokenId2], [])
     ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
   });
 
   it("Tries to take a flashloan using not owned tokens (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user1TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user1TokenId1], [])
     ).to.be.revertedWith("BNFT: caller is not owner");
   });
 
-  it("Takes BAYC flashloan, simulating receiver execution failed (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+  it("Takes flashloan, simulating receiver execution failed (revert expected)", async () => {
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setFailExecution(true);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("BNFT: invalid flashloan executor return");
   });
 
   it("tries to take a flashloan using non contract address as receiver (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
-    await expect(bBYAC.connect(users[0].signer).flashLoan(users[1].address, [user0TokenId1], [])).to.be.revertedWith(
+    await expect(bBAYC.connect(users[0].signer).flashLoan(users[1].address, [user0TokenId1], [])).to.be.revertedWith(
       ""
     );
   });
 
   it("Tries to take a flashloan reentry BNFT contract mint (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setSimulateCallBNFT(1, 0);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("BNFT: exist token");
   });
 
   it("Tries to take a flashloan reentry BNFT burn (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setSimulateCallBNFT(2, 0);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("BNFT: caller is not minter");
   });
 
   it("Tries to take a flashloan reentry BNFT flashloan with mode 3 (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setSimulateCallBNFT(3, user0TokenId1);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("BNFT: caller is not owner");
   });
 
   it("Tries to take a flashloan reentry BNFT flashloan with mode 4 (revert expected)", async () => {
-    const { users, pool, bayc, bBYAC } = testEnv;
+    const { users, pool, bayc, bBAYC } = testEnv;
 
     await _mockFlashLoanReceiver.setSimulateCallBNFT(4, user0TokenId2);
 
     await expect(
-      bBYAC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
+      bBAYC.connect(users[0].signer).flashLoan(_mockFlashLoanReceiver.address, [user0TokenId1], [])
     ).to.be.revertedWith("BNFT: caller is not owner");
   });
 });
--- test/loan-modifiers.spec.ts
+++ test/loan-modifiers.spec.ts
@@ -6,34 +6,34 @@ makeSuite("LendPoolLoan: Modifiers", (testEnv: TestEnv) => {
   const { CT_CALLER_MUST_BE_LENDING_POOL } = ProtocolErrors;
 
   it("Tries to invoke initNft not being the Pool", async () => {
-    const { deployer, bayc, bBYAC, bWETH, loan } = testEnv;
-    await expect(loan.initNft(bayc.address, bBYAC.address)).to.be.revertedWith(CT_CALLER_MUST_BE_LENDING_POOL);
+    const { deployer, bayc, bBAYC, bWETH, loan } = testEnv;
+    await expect(loan.initNft(bayc.address, bBAYC.address)).to.be.revertedWith(CT_CALLER_MUST_BE_LENDING_POOL);
   });
 
   it("Tries to invoke createLoan not being the Pool", async () => {
-    const { deployer, bayc, bBYAC, bWETH, loan } = testEnv;
+    const { deployer, bayc, bBAYC, bWETH, loan } = testEnv;
     await expect(
-      loan.createLoan(deployer.address, deployer.address, bayc.address, "1", bBYAC.address, bWETH.address, "1", "1")
+      loan.createLoan(deployer.address, deployer.address, bayc.address, "1", bBAYC.address, bWETH.address, "1", "1")
     ).to.be.revertedWith(CT_CALLER_MUST_BE_LENDING_POOL);
   });
 
   it("Tries to invoke updateLoan not being the Pool", async () => {
-    const { deployer, bayc, bBYAC, bWETH, loan } = testEnv;
+    const { deployer, bayc, bBAYC, bWETH, loan } = testEnv;
     await expect(loan.updateLoan(deployer.address, "1", "1", "0", "1")).to.be.revertedWith(
       CT_CALLER_MUST_BE_LENDING_POOL
     );
   });
 
   it("Tries to invoke repayLoan not being the Pool", async () => {
-    const { deployer, bayc, bBYAC, bWETH, loan } = testEnv;
-    await expect(loan.repayLoan(deployer.address, "1", bBYAC.address)).to.be.revertedWith(
+    const { deployer, bayc, bBAYC, bWETH, loan } = testEnv;
+    await expect(loan.repayLoan(deployer.address, "1", bBAYC.address)).to.be.revertedWith(
       CT_CALLER_MUST_BE_LENDING_POOL
     );
   });
 
   it("Tries to invoke liquidateLoan not being the Pool", async () => {
-    const { deployer, bayc, bBYAC, bWETH, loan } = testEnv;
-    await expect(loan.liquidateLoan(deployer.address, "1", bBYAC.address)).to.be.revertedWith(
+    const { deployer, bayc, bBAYC, bWETH, loan } = testEnv;
+    await expect(loan.liquidateLoan(deployer.address, "1", bBAYC.address)).to.be.revertedWith(
       CT_CALLER_MUST_BE_LENDING_POOL
     );
   });
