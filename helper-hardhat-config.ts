c0fb48120d95870fa5441d61d7fb6e91c24a8f4b
wenyuangui <wenyuangui@yeah.net>
2021-11-05 18:42:26 +0800

    remove useless network (kovan,ropsten)

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -29,36 +29,23 @@ export const NETWORKS_RPC_URL: iParamsPerNetwork<string> = {
   [eEthereumNetwork.rinkeby]: ALCHEMY_KEY
     ? `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`
     : `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-  [eEthereumNetwork.kovan]: ALCHEMY_KEY
-    ? `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KEY}`
-    : `https://kovan.infura.io/v3/${INFURA_KEY}`,
-  [eEthereumNetwork.ropsten]: ALCHEMY_KEY
-    ? `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`
-    : `https://ropsten.infura.io/v3/${INFURA_KEY}`,
   [eEthereumNetwork.main]: ALCHEMY_KEY
     ? `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`
     : `https://mainnet.infura.io/v3/${INFURA_KEY}`,
   [eEthereumNetwork.coverage]: "http://localhost:8555",
   [eEthereumNetwork.hardhat]: "http://localhost:8545",
-  [eEthereumNetwork.buidlerevm]: "http://localhost:8545",
 };
 
 export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
   [eEthereumNetwork.rinkeby]: 65 * GWEI,
-  [eEthereumNetwork.kovan]: 1 * GWEI,
-  [eEthereumNetwork.ropsten]: 65 * GWEI,
   [eEthereumNetwork.main]: 65 * GWEI,
   [eEthereumNetwork.coverage]: 65 * GWEI,
   [eEthereumNetwork.hardhat]: 65 * GWEI,
-  [eEthereumNetwork.buidlerevm]: 65 * GWEI,
 };
 
 export const BLOCK_TO_FORK: iParamsPerNetwork<number | undefined> = {
   [eEthereumNetwork.main]: 12406069,
   [eEthereumNetwork.rinkeby]: undefined,
-  [eEthereumNetwork.kovan]: undefined,
-  [eEthereumNetwork.ropsten]: undefined,
   [eEthereumNetwork.coverage]: undefined,
   [eEthereumNetwork.hardhat]: undefined,
-  [eEthereumNetwork.buidlerevm]: undefined,
 };
