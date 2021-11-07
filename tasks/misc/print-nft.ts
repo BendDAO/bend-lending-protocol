9001606abc669f936a2ed3e65f77d8ba465b8493
Thanos Wen <thanoswen@protonmail.com>
2021-11-07 14:09:11 +0800

    add some debug script for deploying

---
+++ tasks/misc/print-nft.ts
@@ -0,0 +1,45 @@
+import { task } from "hardhat/config";
+import { ConfigNames, loadPoolConfig } from "../../helpers/configuration";
+import {
+  getBendProtocolDataProvider,
+  //getLendPoolAddressesProviderRegistry,
+} from "../../helpers/contracts-getters";
+import { getParamPerNetwork } from "../../helpers/contracts-helpers";
+import { DRE } from "../../helpers/misc-utils";
+import { eEthereumNetwork, eNetwork } from "../../helpers/types";
+
+task("print-nft", "Print data of specified nft")
+  .addParam("dataProvider", "Address of BendProtocolDataProvider")
+  .addParam("pool", `Pool name to retrieve configuration, supported: ${Object.values(ConfigNames)}`)
+  .addOptionalParam("asset", `NFT address`)
+  .addOptionalParam("tokenId", `NFT Token ID`)
+  .addOptionalParam("loanId", `NFT Loan ID`)
+  .setAction(async ({ pool, dataProvider, asset, tokenId, loanId }, localBRE) => {
+    await localBRE.run("set-DRE");
+    const network = process.env.FORK ? (process.env.FORK as eNetwork) : (localBRE.network.name as eNetwork);
+    const poolConfig = loadPoolConfig(pool);
+
+    const protocolDataProvider = await getBendProtocolDataProvider(dataProvider);
+
+    let loanData;
+    if (loanId != undefined) {
+      loanData = await protocolDataProvider.getLoanDataByLoanId(loanId);
+    } else {
+      loanData = await protocolDataProvider.getLoanDataByCollateral(asset, tokenId);
+    }
+
+    console.log(`- nft loan data`);
+    const loanFields = [
+      "loanId",
+      "state",
+      "nftAsset",
+      "nftTokenId",
+      "borrower",
+      "reserveAsset",
+      "scaledAmount",
+      "currentAmount",
+    ];
+    loanFields.forEach((field, index) => {
+      console.log(`  - ${field}:`, loanData[field].toString());
+    });
+  });
---
+++ tasks/misc/print-reserve.ts
@@ -0,0 +1,45 @@
+import { task } from "hardhat/config";
+import { ConfigNames, loadPoolConfig } from "../../helpers/configuration";
+import { getBendProtocolDataProvider } from "../../helpers/contracts-getters";
+import { getParamPerNetwork } from "../../helpers/contracts-helpers";
+import { DRE } from "../../helpers/misc-utils";
+import { eEthereumNetwork, eNetwork } from "../../helpers/types";
+
+task("print-reserve", "Print data of specified reserve and user")
+  .addParam("dataProvider", "Address of BendProtocolDataProvider")
+  .addParam("pool", `Pool name to retrieve configuration, supported: ${Object.values(ConfigNames)}`)
+  .addParam("asset", `Reserve address`)
+  .addOptionalParam("user", `User address`)
+  .setAction(async ({ pool, dataProvider, asset, user }, localBRE) => {
+    await localBRE.run("set-DRE");
+    const network = process.env.FORK ? (process.env.FORK as eNetwork) : (localBRE.network.name as eNetwork);
+    const poolConfig = loadPoolConfig(pool);
+
+    const protocolDataProvider = await getBendProtocolDataProvider(dataProvider);
+
+    const reserveData = await protocolDataProvider.getReserveData(asset);
+
+    console.log(`- reserve data`);
+    const reserveFields = [
+      "availableLiquidity",
+      "totalVariableDebt",
+      "liquidityRate",
+      "variableBorrowRate",
+      "liquidityIndex",
+      "variableBorrowIndex",
+      "lastUpdateTimestamp",
+    ];
+    reserveFields.forEach((field, index) => {
+      console.log(`  - ${field}:`, reserveData[field].toString());
+    });
+
+    if (user != undefined) {
+      const userReserveData = await protocolDataProvider.getUserReserveData(asset, user);
+
+      console.log(`- user reserve data`);
+      const userReserveFields = ["currentBTokenBalance", "currentVariableDebt", "scaledVariableDebt"];
+      userReserveFields.forEach((field, index) => {
+        console.log(`  - ${field}:`, userReserveData[field].toString());
+      });
+    }
+  });
