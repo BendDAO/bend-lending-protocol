cff4d16e3460ee9dedad278d9028ce80c66a5ae9
Thor Seldon <thorseldon@protonmail.com>
2024-09-20 08:45:29 +0800

    deploy new interest rate for bip62

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -47,7 +47,7 @@ export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
   [eEthereumNetwork.sepolia]: 35 * GWEI,
   [eEthereumNetwork.goerli]: 65 * GWEI,
   [eEthereumNetwork.rinkeby]: 65 * GWEI,
-  [eEthereumNetwork.main]: 15 * GWEI,
+  [eEthereumNetwork.main]: 10 * GWEI,
   [eEthereumNetwork.coverage]: 65 * GWEI,
   [eEthereumNetwork.hardhat]: 65 * GWEI,
   [eEthereumNetwork.localhost]: 65 * GWEI,
