9615ae85dac5860ecd40f0861de82a5f21fee88b
Thor Seldon <thorseldon@protonmail.com>
2025-03-02 10:31:22 +0800

    deploy new interest rate contracts

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -44,10 +44,10 @@ export const NETWORKS_RPC_URL: iParamsPerNetwork<string> = {
 };
 
 export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
-  [eEthereumNetwork.sepolia]: 35 * GWEI,
+  [eEthereumNetwork.sepolia]: 15 * GWEI,
   [eEthereumNetwork.goerli]: 65 * GWEI,
   [eEthereumNetwork.rinkeby]: 65 * GWEI,
-  [eEthereumNetwork.main]: 15 * GWEI,
+  [eEthereumNetwork.main]: 5 * GWEI,
   [eEthereumNetwork.coverage]: 65 * GWEI,
   [eEthereumNetwork.hardhat]: 65 * GWEI,
   [eEthereumNetwork.localhost]: 65 * GWEI,
