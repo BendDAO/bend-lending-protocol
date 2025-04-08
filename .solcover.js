552797d87f50f636b1222c0b188020866a39e063
Thor Seldon <thorseldon@protonmail.com>
2025-04-08 21:02:21 +0800

    updating gh workflows files

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,6 @@
 const accounts = require(`./test-wallets.js`).accounts;
 
 module.exports = {
-  client: require('ganache-cli'),
   skipFiles: ['./mocks', './interfaces'],
   mocha: {
     enableTimeouts: false,
